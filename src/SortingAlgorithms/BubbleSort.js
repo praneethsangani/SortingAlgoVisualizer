810d998b7cfbcedf05f2014fc062689ab40b3965
Praneeth Sangani <prs79@pitt.edu>
2020-01-17 18:42:05 -0500

    MergeSort BottomUp & Top Down, Quick Sort Implemented

--- src/SortingAlgorithms/BubbleSort.js
+++ src/SortingAlgorithms/BubbleSort.js
@@ -3,11 +3,11 @@ export async function bubbleSort(array, ANIMATION_SPEED_MS) {
     for (let i = 0; i < array.length; i++) {
         for (let j = 0; j < array.length - i - 1; j++) {
             if (array[j] > array[j + 1]) {
-                swapValues(array, bars, j, ANIMATION_SPEED_MS);
+                swapValues(array, bars, j);
                 await new Promise(r => setTimeout(r, ANIMATION_SPEED_MS));
             } else {
                 bars[j + 1].style.backgroundColor = 'red';
-                bars[j].style.backgroundColor = '#0f4c75';
+                bars[j].style.backgroundColor = 'darkturquoise';
                 await new Promise(r => setTimeout(r, ANIMATION_SPEED_MS));
             }
             if (j + 1 === array.length - i - 1) {
@@ -20,7 +20,7 @@ export async function bubbleSort(array, ANIMATION_SPEED_MS) {
 }
 
 function swapValues(array, bars, index) {
-    const barValues = document.getElementsByClassName('barValue');
+    // const barValues = document.getElementsByClassName('barValue');
     let temp_height = array[index + 1];
     array[index + 1] = array[index];
     array[index] = temp_height;
@@ -28,8 +28,8 @@ function swapValues(array, bars, index) {
     bars[index + 1].style.height = bars[index].style.height;
     bars[index].style.height = temp_height;
     bars[index + 1].style.backgroundColor = 'red';
-    bars[index].style.backgroundColor = '#0f4c75';
-    temp_height = barValues[index + 1].textContent;
-    barValues[index + 1].textContent = barValues[index].textContent;
-    barValues[index].textContent = temp_height;
+    bars[index].style.backgroundColor = 'darkturquoise';
+    // temp_height = barValues[index + 1].textContent;
+    // barValues[index + 1].textContent = barValues[index].textContent;
+    // barValues[index].textContent = temp_height;
 }
--- src/SortingAlgorithms/MergeSortTopDown.js
+++ src/SortingAlgorithms/MergeSortTopDown.js
@@ -1,33 +1,30 @@
 export async function mergeSortTopDown(mainArray, startIndex, endIndex, tempArray) {
     if (startIndex === endIndex) return;
     const middleIdx = Math.floor((startIndex + endIndex) / 2);
-    await mergeSortTopDown(tempArray, startIndex, middleIdx, mainArray);
-    await mergeSortTopDown(tempArray, middleIdx + 1, endIndex, mainArray);
-    return await doMerge(mainArray, startIndex, middleIdx, endIndex, tempArray);
+    await mergeSortTopDown(tempArray, startIndex, middleIdx, mainArray);             // Split left side into subsections
+    await mergeSortTopDown(tempArray, middleIdx + 1, endIndex, mainArray); // Split right side into subsections
+    return await doMerge(mainArray, tempArray, startIndex, middleIdx, endIndex);     // Merge the subsections back together
 }
 
-async function doMerge(mainArray, startIndex, middleIdx, endIndex, tempArray) {
+async function doMerge(mainArray, tempArray, startIndex, middleIndex, endIndex) {
     const bars = document.getElementsByClassName('array-bar');
     let mainIndex = startIndex;
     let leftIndex = startIndex;
-    let rightIndex = middleIdx + 1;
+    let rightIndex = middleIndex + 1;
 
-    while (leftIndex <= middleIdx && rightIndex <= endIndex) {
-        changeBarColor(bars, leftIndex, rightIndex, 'red');
+    while (leftIndex <= middleIndex && rightIndex <= endIndex) {
+        await displayAnimations(bars, leftIndex, rightIndex);
+
         if (tempArray[leftIndex] <= tempArray[rightIndex]) {
             mainArray[mainIndex] = tempArray[leftIndex];
             bars[mainIndex++].style.height = tempArray[leftIndex++] + 'px';
-            await new Promise(r => setTimeout(r, 8));
-            changeBarColor(bars, leftIndex-1, rightIndex, 'darkturquoise');
         } else {
             mainArray[mainIndex] = tempArray[rightIndex];
             bars[mainIndex++].style.height = tempArray[rightIndex++] + 'px';
-            await new Promise(r => setTimeout(r, 8));
-            await changeBarColor(bars, leftIndex, rightIndex-1, 'darkturquoise');
         }
     }
 
-    while (leftIndex <= middleIdx) {
+    while (leftIndex <= middleIndex) {
         mainArray[mainIndex] = tempArray[leftIndex];
         bars[mainIndex++].style.height = tempArray[leftIndex++] + 'px';
 
@@ -37,12 +34,14 @@ async function doMerge(mainArray, startIndex, middleIdx, endIndex, tempArray) {
         bars[mainIndex++].style.height = tempArray[rightIndex++] + 'px';
     }
 
-    console.log(mainArray);
     return mainArray;
 }
 
 // Change the color of the bars at index1 && index2
-function changeBarColor(bars, index1, index2, color) {
-    bars[index1].style.backgroundColor = color;
-    bars[index2].style.backgroundColor = color;
+async function displayAnimations(bars, index1, index2) {
+    bars[index1].style.backgroundColor = 'red';
+    bars[index2].style.backgroundColor = 'red';
+    await new Promise(r => setTimeout(r, 20));
+    bars[index1].style.backgroundColor = 'darkturquoise';
+    bars[index2].style.backgroundColor = 'darkturquoise';
 }
